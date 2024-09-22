9a80206f5f6d4e895892480edf4cb429c3d4c6d4
ye1sultan <niyaztaye@gmail.com>
2024-09-23 02:29:20 +0500

    feat: create types for user and blog

---
+++ src/shared/types/blog.ts
@@ -0,0 +1,6 @@
+export interface IBlog {
+  name: string;
+  date: string;
+  link: string;
+  description?: string;
+}
---
+++ src/shared/types/me.ts
@@ -0,0 +1,8 @@
+export interface IMe {
+  name: string;
+  about: string;
+  links: {
+    name: string;
+    link: string;
+  }[];
+}
