4b15ebe0fa15ce7bec70622389ba2c57b0a973b2
ye1sultan <niyaztaye@gmail.com>
2024-09-23 01:15:45 +0500

    feat: change infinite to carousel

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,9 +1,5 @@
-"use client";
-
-import { Infinite } from "@/shared/components/custom/carousel";
-import { Title } from "@/shared/components/custom/title";
-import MemoArrow from "@/shared/icons/arrow";
-import Link from "next/link";
+import { Carousel } from "@/shared/components/carousel";
+import { Title } from "@/shared/components/title";
 
 const Home = () => {
   return (
@@ -12,24 +8,7 @@ const Home = () => {
       <p>
         {`I do my best at night when everything is quiet and I can really dive into coding. I'm convinced my productivity has a direct correlation with caffeine levels, so with Red Bull keeping me energized, I feel unstoppable. And it's all about finding that perfect workflow that lets me get things done faster and more effectively.`}
       </p>
-      <Infinite />
-      <div className="flex gap-6">
-        <Link href="https://t.me/yelsultan" className="flex gap-1 items-center">
-          <MemoArrow className="text-xl rotate-45" /> telegram
-        </Link>
-        <Link
-          href="https://github.com/ye1sultan"
-          className="flex gap-1 items-center"
-        >
-          <MemoArrow className="text-xl rotate-45" /> github
-        </Link>
-        <Link
-          href="https://www.instagram.com/yels667/"
-          className="flex gap-1 items-center"
-        >
-          <MemoArrow className="text-xl rotate-45" /> instagram
-        </Link>
-      </div>
+      <Carousel />
     </div>
   );
 };
