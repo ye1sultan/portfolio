ef4dfe6714bf48c7a561850073dd5ef6fe44e3b7
ye1sultan <niyaztaye@gmail.com>
2024-09-23 01:13:01 +0500

    feat: add experience page to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,6 @@
 export const baseUrl = "https://portfolio-yelsultan.vercel.app";
 
-const staticRoutes = ["/home", "/blog"];
+const staticRoutes = ["/home", "/experience", "blog"];
 
 export default async function sitemap() {
   const currentDate = new Date().toISOString().split("T")[0];
