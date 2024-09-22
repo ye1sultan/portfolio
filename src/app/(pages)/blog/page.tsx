56fc2718f0eb9215d4954a65c200c7856837a2e3
ye1sultan <niyaztaye@gmail.com>
2024-09-23 01:15:15 +0500

    feat: remove custom folder, move to parent component folder, renmae blog to experience

--- src/app/(pages)/blog/page.tsx
+++
@@ -1,5 +0,0 @@
-const Blog = () => {
-  return <>Blog</>;
-};
-
-export default Blog;
--- src/shared/components/custom/title/index.tsx
+++
@@ -1,3 +0,0 @@
-export const Title = ({ text }: { text: string }) => {
-  return <h1 className="text-2xl font-semibold">{text}</h1>;
-};
