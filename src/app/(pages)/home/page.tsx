9fca12e13d0c7a48d6dfb5049be74ce40b155bb0
ye1sultan <niyaztaye@gmail.com>
2024-09-23 02:30:05 +0500

    feat: use stored data

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,13 +1,12 @@
 import { Carousel } from "@/shared/components/carousel";
 import { Title } from "@/shared/components/title";
+import { me } from "@/shared/data/const";
 
 const Home = () => {
   return (
     <div className="flex flex-col gap-8">
-      <Title text="Niyaztay Yelsultan" />
-      <p>
-        {`I do my best at night when everything is quiet and I can really dive into coding. I'm convinced my productivity has a direct correlation with caffeine levels, so with Red Bull keeping me energized, I feel unstoppable. And it's all about finding that perfect workflow that lets me get things done faster and more effectively.`}
-      </p>
+      <Title text={me.name} />
+      <p>{me.about}</p>
       <Carousel />
     </div>
   );
--- src/shared/components/carousel/index.tsx
+++ src/shared/components/carousel/index.tsx
@@ -1,68 +1,25 @@
+import { me } from "@/shared/data/const";
 import Link from "next/link";
 
 export const Carousel = () => {
   return (
     <div className="mb-10 w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_20px,_black_calc(100%-50px),transparent_100%)]">
       <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_span]:max-w-none animate-infinite-scroll tracking-wider">
-        <li>
-          <Link href="https://nodejs.org/en">node.js</Link>
-        </li>
-        <li>
-          <Link href="https://expressjs.com/">express.js</Link>
-        </li>
-        <li>
-          <Link href="">react</Link>
-        </li>
-        <li>
-          <Link href="">next.js</Link>
-        </li>
-        <li>
-          <Link href="">tailwind</Link>
-        </li>
-        <li>
-          <Link href="">scss</Link>
-        </li>
-        <li>
-          <Link href="">jotai</Link>
-        </li>
-        <li>
-          <Link href="">tanstack</Link>
-        </li>
-        <li>
-          <Link href="">redux</Link>
-        </li>
+        {me.links.map((item, index) => (
+          <li key={index}>
+            <Link href={item.link}>{item.name}</Link>
+          </li>
+        ))}
       </ul>
       <ul
         className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_span]:max-w-none animate-infinite-scroll tracking-wider"
         aria-hidden="true"
       >
-        <li>
-          <Link href="https://nodejs.org/en">node.js</Link>
-        </li>
-        <li>
-          <Link href="https://expressjs.com/">express.js</Link>
-        </li>
-        <li>
-          <Link href="">react</Link>
-        </li>
-        <li>
-          <Link href="">next.js</Link>
-        </li>
-        <li>
-          <Link href="">tailwind</Link>
-        </li>
-        <li>
-          <Link href="">scss</Link>
-        </li>
-        <li>
-          <Link href="">jotai</Link>
-        </li>
-        <li>
-          <Link href="">tanstack</Link>
-        </li>
-        <li>
-          <Link href="">redux</Link>
-        </li>
+        {me.links.map((item, index) => (
+          <li key={index}>
+            <Link href={item.link}>{item.name}</Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
