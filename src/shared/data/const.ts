124a1cd7045b586bf724a6feaac13bf2f5351f69
ye1sultan <niyaztaye@gmail.com>
2024-11-28 19:20:31 +0500

    feat: change and updated expreience texts

--- src/shared/data/const.ts
+++ src/shared/data/const.ts
@@ -51,25 +51,33 @@ export const me: IMe = {
 
 export const experience: IBlog[] = [
   {
-    name: "Qtap",
-    date: "Jan 24 - Jun 24",
-    link: "https://qtap.kz/",
-    description:
-      "Web app, that allows users to store personal info in customizable tiles (video, audio, text, links, photos) with a minimalistic design. I used Next.js, Tailwind CSS, Jotai, and TanStack Query to create a responsive and user-friendly experience.",
+    name: "EasyRent",
+    date: "Aug 24 - Present",
+    link: "https://easyrent.one",
+    description: [
+      "Developed a responsive and user-friendly car rental platform using Next.js, Tailwind CSS, Jotai, and TanStack Query to streamline the browsing and selection experience.",
+      "Optimized data fetching and state management to improve performance and ensure seamless user interactions across the platform.",
+    ],
   },
   {
     name: "Finance.kz",
     date: "Jun 24 - Present",
     link: "https://finance.kz/",
-    description:
-      "Platform that provides listings and information about credits, microcredits, mortgages, deposits, as well as credit and debit cards. I used Next.js, SCSS, and Redux to create a responsive and efficient site that helps users easily navigate financial options.",
+    description: [
+      "Designed and developed a custom admin panel from scratch, streamlining workflows and enhancing the user experience by making product creation, editing, and management up to 50% more efficient.",
+      "Mentored and guided all interns, providing comprehensive support in feature development and enhancing their mastery of front-end best practices",
+    ],
   },
   {
-    name: "EasyRent",
-    date: "Aug 24 - Present",
-    link: "https://easyrent.one",
-    description:
-      "Web app for renting various cars, allowing users to browse and select vehicles through an intuitive interface. I utilized Next.js, Tailwind CSS, Jotai, and TanStack Query to develop a responsive and user-friendly platform that streamlines the car rental experience.",
+    name: "Qtap",
+    date: "Jan 24 - Jun 24",
+    link: "https://qtap.kz/",
+    description: [
+      "Optimized state management by leveraging Jotai and TanStack Query, improving data handling efficiency across the application.",
+      "Implemented a fully responsive design using Next.js and Tailwind CSS, enhancing accessibility and user experience across all devices.",
+      "Covered 60% of the web application with unit tests, ensuring code reliability and maintainability.",
+      "Developed 100% of all email templates using React Email for consistent and dynamic communication.",
+    ],
   },
 ];
 
@@ -78,8 +86,9 @@ export const education: IBlog[] = [
     name: "Suleyman Demirel University",
     date: "Sep 19 - Jun 23",
     link: "https://sdu.edu.kz/language/en/",
-    description:
-      "I gained a solid foundation in programming, algorithms, data structures, and software development. I learned how to problem-solve using various programming languages, work with databases, and understand computer systems on a deeper level.",
+    description: [
+      "Gained a solid foundation in programming, algorithms, data structures, and software development. I learned how to problem-solve using various programming languages, work with databases, and understand computer systems on a deeper level.",
+    ],
   },
   {
     name: "JavaScript + React • Udemy",
