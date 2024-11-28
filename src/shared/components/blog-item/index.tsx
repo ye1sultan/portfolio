fe5ec8badee53bf6092ed55cd7e3f7c574c6c15c
ye1sultan <niyaztaye@gmail.com>
2024-11-28 19:20:48 +0500

    feat: change description render

--- src/shared/components/blog-item/index.tsx
+++ src/shared/components/blog-item/index.tsx
@@ -5,12 +5,22 @@ export const BlogItem = ({ name, date, link, description }: IBlog) => {
   return (
     <div>
       <div className="flex w-full gap-2 justify-between items-start mb-4">
-        <Link href={link} target="_blank" className="text-xl">
-          • {name}
+        <Link
+          href={link}
+          target="_blank"
+          className="text-xl underline underline-offset-4"
+        >
+          {name}
         </Link>
         <p className="text-neutral-300 shrink-0">{date}</p>
       </div>
-      <p>{description}</p>
+      <ul className="flex flex-col gap-1">
+        {description?.map((item, index) => (
+          <li key={index} className="list-inside list-disc">
+            {item}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
