4ff5d59dd15ae11861f4065fbc1a7ba17f875ea8
ye1sultan <niyaztaye@gmail.com>
2025-05-05 13:56:25 +0500

    feat: update stack links

--- src/shared/data/me.ts
+++ src/shared/data/me.ts
@@ -6,6 +6,10 @@ export const me: IMe = {
     "I do my best work when it’s quiet and I can get in the zone — usually late at night, headphones on, Red Bull nearby, deep in code. I’m all about flow and efficiency: building fast, clean, and scalable systems without the fluff. I’m constantly refining my workflow to move quicker and ship smarter. Whether it’s diving into new tech or pushing a product over the finish line, I bring energy, focus, and a high bar for quality to everything I build.",
   links: [
     {
+      name: "typescript",
+      link: "https://www.typescriptlang.org/",
+    },
+    {
       name: "node.js",
       link: "https://nodejs.org/en",
     },
@@ -26,10 +30,6 @@ export const me: IMe = {
       link: "https://tailwindcss.com/",
     },
     {
-      name: "sass",
-      link: "https://sass-lang.com/",
-    },
-    {
       name: "scss",
       link: "https://sass-lang.com/",
     },
@@ -42,8 +42,16 @@ export const me: IMe = {
       link: "https://redux.js.org/",
     },
     {
-      name: "tanstack",
-      link: "https://tanstack.com/",
+      name: "graphql",
+      link: "https://graphql.org/",
+    },
+    {
+      name: "react-native",
+      link: "https://reactnative.dev/",
+    },
+    {
+      name: "electron",
+      link: "https://www.electronjs.org/",
     },
   ],
 };
