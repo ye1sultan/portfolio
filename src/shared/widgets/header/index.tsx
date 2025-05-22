9bf46c8993b23e1a890fa15bc2fb629b3d10c25d
ye1sultan <niyaztaye@gmail.com>
2025-05-22 11:48:30 +0500

    feat: make active header button have bottom border

--- src/shared/widgets/header/index.tsx
+++ src/shared/widgets/header/index.tsx
@@ -1,12 +1,28 @@
+"use client";
+
+import { cn } from "@/shared/lib/utils";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export const Header = () => {
+  const pathname = usePathname();
+
+  const isHome = pathname === "/" || pathname === "/home";
+  const isExperience = pathname === "/experience";
+  const isEducation = pathname === "/education";
+
   return (
     <header>
       <nav className="flex items-center gap-6">
-        <Link href="/home">home</Link>
-        <Link href="/experience">experience</Link>
-        <Link href="/education">education</Link>
+        <Link href="/home" className={cn(isHome ? "border-b" : "")}>
+          home
+        </Link>
+        <Link href="/experience" className={cn(isExperience ? "border-b" : "")}>
+          experience
+        </Link>
+        <Link href="/education" className={cn(isEducation ? "border-b" : "")}>
+          education
+        </Link>
       </nav>
     </header>
   );
